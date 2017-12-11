b382982265f563fe895a43883119e6c5cc1925f6
kaskoa <34346507+kaskoa@users.noreply.github.com>
2017-12-11 22:04:47 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,18 @@
+﻿random <- function(min, max){
+  if(min > max){
+    return ("Максимальне значення більше за мінімальне")
+  }
+  if(min == max){
+    value <- min
+  }
+  value <- sample(min:max, 1)
+  
+  return (paste("Випадкове значення: ", value))
+}
+shinyServer(
+  function(input, output) {
+    output$text <- eventReactive(input$button, {
+      random(input$min, input$max)
+    })
+  }
+)
---
+++ ui.R
@@ -0,0 +1,15 @@
+﻿library("shiny")
+
+shinyUI(pageWithSidebar(
+  headerPanel(""),
+  
+  sidebarPanel(
+    numericInput("min", "Мінімальне число", min = 0, max = 100, value = 10),
+    hr(),
+    numericInput("max", "Максимальне число", min = 0, max = 100, value = 25),
+    actionButton("button", "Згенерувати", width = "100%")
+  ),
+  mainPanel(
+    textOutput("text")
+  )
+))
